237d26d4f44657416acfe18a3acbcc8b37e1ea92
Daniel Adesanya <coltcopy@gmail.com>
2022-06-27 17:20:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let mainMenu = document.getElementsByClassName("button-container")
+const menuBtn = document.getElementsByClassName("mobileMenuBtn")
+var menuDisplay = document.querySelector(".desktopMenu")
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+      menuDisplay.classList.toggle("show")
+
+  }
+
+
+
+
+
